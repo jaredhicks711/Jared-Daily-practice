c89d83ce0c42ced5d1a1f7a89c62d61f69d57e41
Jared Hicks <JHicks@alterramtnco.com>
2018-05-11 10:28:05 -0600

    commiting project euler number 3

--- project_euler_3.js
+++ project_euler_3.js
@@ -1,3 +1,20 @@
 //project euler problem #2
 //Find the largest prime factor of a composite number
-//unfinished
+//finished
+let largestPrime = 1;
+function findLargestPrime(num) {
+    let currentNum = num;
+    while (largestPrime <= currentNum) {
+        for (let i = 2; i <= currentNum; i++) {
+            if (currentNum % i == 0) {
+                if (i > largestPrime) {
+                    largestPrime = i;
+                }
+                currentNum /= i;
+                break;
+            }
+        }
+    }
+    return largestPrime;
+}
+console.log(findLargestPrime(600851475143));
