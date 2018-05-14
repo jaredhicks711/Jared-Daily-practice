0cd1dcde0c0bca2bd285cf8f97f15205b281cc12
Jared Hicks <JHicks@alterramtnco.com>
2018-05-14 10:13:24 -0600

    minor changes

--- project_euler_4.js
+++ project_euler_4.js
@@ -1,13 +1,23 @@
 //project euler problem #4
 //Largest palindrome product
-//unfinished
+//finished
+let largestPali = 0;
 function isPalindrome(num){
     let num1 = num.toString();
-    let reverseString;
+    let reverseString ="";
     for(let i = num1.length-1;i>=0;i--){
         reverseString+= num1.charAt(i);
     }
-    console.log(reverseString);
    return(reverseString === num1);
 }
-console.log(isPalindrome(131));
+function paliProduct(min, max){
+    for(let a = min;a<=max; a++){
+        for(let b = min; b<=max; b++){
+            if(isPalindrome(a*b) && (a*b) >= largestPali){
+                largestPali = (a*b);
+            }
+        }
+    }
+    return largestPali;
+}
+console.log(paliProduct(100, 999));
